8fff55f47f4b626c146a8c2082a84de99f3aaca2
spkasu2405 <spkasu2405@gmail.com>
2025-10-02 11:22:12 -0500

    Update database.sql

--- scripts/database.sql
+++ scripts/database.sql
@@ -1,1 +1,39 @@
+/*
+--=======================================
+creating a database and schema
+==========================================
+Script Purpose:
+the script creates a new database name "Datawarehouse" after checking it already exists.
+if the database exists it drops and recreate
+aditionally the script set up 3 SCHEMAS( bronze, silver, gold) with in the database datawarehouse.
 
+Warning: 
+Running the database will drop the entire datawarehouse database  if exists.
+*/
+
+
+
+use master;
+=============================================
+--drop and recreate database if exists
+=============================================
+if exists(select 1 from sys.databases where name= 'datawarehouse')
+begin
+   alter database datawarehouse set single_user with rollback immediate;
+   Drop database datawarehouse
+   end;
+   go
+--====================
+Create a database
+--=====================
+create database datawarehouse;
+
+use datawarehouse;
+ ====================
+ create a schema
+ =====================
+ create schema bronze
+ go
+ create schema silver
+  go
+  create schema gold
